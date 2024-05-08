908f9cb36d06939ce7a1a9857dee10cdda7f05c6
ateafack-nora <64075437+ateafack-nora@users.noreply.github.com>
2024-05-08 11:33:25 +0100

    inserting the variables in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const myProfileData = {
+    name: "Ateafack Nora",
+    profilePicUrl: `${process.env.PUBLIC_URL}/my-profile-pic.jpg`,
+    age: 90,
+    bio: "Interested in React",
+    birthday: "July 16",
+    interests: ["programming", "Cyber Security", "Graphic Design"],
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Friend Tracker</h1>
+      <div>
+        <img
+          src={myProfileData.profilePicUrl}
+          alt={`${myProfileData.name} smiling`}
+          height="200"
+        />
+        <h2>My Profile</h2>
+        <h3>Name</h3>
+        <p>{myProfileData.name}</p>
+        <h3>Age</h3>
+        <p>{myProfileData.age}</p>
+        <h3>Bio</h3>
+        <p>{myProfileData.bio}</p>
+        <h3>Birthday</h3>
+        <p>{myProfileData.birthday}</p>
+        <h3>Interests</h3>
+        <p>{myProfileData.interests.join(",")}</p>
+      </div>
     </div>
   );
 }
